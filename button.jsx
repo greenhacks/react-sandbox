4f9926ac8c4e0fd838a46e9f939a1a7527e2f860
Sam <stmiyamoto10@gmail.com>
2022-01-13 17:15:02 -0800

    Initial commit

---
+++ button.jsx
@@ -0,0 +1,21 @@
+//A simple React click counter
+
+function ClickCounter(props) {
+  const [currentCount, setCurrentCount] = React.useState(props.initialCount);
+  return (
+    <div>
+      <div>{currentCount}</div>
+      <ClickComponent buttonClick={() => setCurrentCount(currentCount + 1)} />
+    </div>
+  );
+}
+
+function ClickComponent({buttonClick}) {
+  return (
+      <button type="button" onClick={buttonClick}>
+        Click here to count many people you shared your results with
+      </button>
+  );
+}
+
+ReactDOM.render(<ClickCounter initialCount={0}/>, document.querySelector('#root'));
